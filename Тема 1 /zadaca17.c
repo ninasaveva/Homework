88c896ac163cb247b1e1f21e0e16fb98ab2957a1
Nina <144626196+ninasaveva@users.noreply.github.com>
2023-09-14 16:55:07 +0200

    Create zadaca17.c

---
+++ Тема 1 /zadaca17.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+//Задача 17
+//Напиши програма каде што преку тастатура ќе може да се внесат два броеви (a,x).
+//Да се пресмета изразот и да се отпечати резултатот (y) на екран.
+//Формула: у = sqrt(a^x + 2*x^3/5a/x)
+
+int main ()
+
+{
+    float a, x;
+    scanf("%f", &a);
+    scanf("%f", &x);
+
+    float y;
+    y = sqrt((pow(a,x) + 2 * pow(x,3) / 5 * a / x));
+
+    printf("The result is: %.2f",y);
+
+
+    return 0;
+}
