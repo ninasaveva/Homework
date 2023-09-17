c3afedf89fd54bab0f7ac2bf4cf2a4ee008f207c
Nina <144626196+ninasaveva@users.noreply.github.com>
2023-09-17 13:52:15 +0200

    Update zadaca8.c

--- Тема 1 /zadaca8.c
+++ Тема 1 /zadaca8.c
@@ -5,19 +5,19 @@
 //Напиши програма каде што преку тастатура ќе може да се внесат три броеви
 //(a, b, h). Пронајди ја плоштината (A) според наведената формула и отпечати го
 //резултатот на екран.
-// Формула: a+b/2 * h
+// Формула: (a+b)/2 * h
 
 int main ()
 
 {
-    int a, b, h;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    scanf("%d", &h);
+    float a, b, h;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &h);
 
-    int A = a+b/2 * h;
+    float A = (a+b)/2 * h;
 
-    printf("The result is: %d", A);
+    printf("The result is: %.2f", A);
 
     return 0;
 }
