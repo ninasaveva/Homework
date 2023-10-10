8174ccd8e6c1e7b9b8fd06ccf10af0725774f67c
Nina <144626196+ninasaveva@users.noreply.github.com>
2023-10-10 20:18:29 +0200

    Create Zadaca8.cpp

---
+++ Тема 3 C++/Zadaca8.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+//8. Напиши програма каде преку тастатура ќе може да се внесат два цели броеви.
+//Ако двата броеви се позитивни да се пресмета квадрат од нивната сума, во
+//спротивно да се пресмета сума од нивните квадрати.
+//Резултатот да се отпечати на екран.
+
+using namespace std;
+
+int main()
+
+{
+    int number1, number2;
+    cin>>number1>>number2;
+
+    int result;
+    int sum = number1 * number2;
+
+    if(number1 > 0 && number2 > 0)
+    {
+        result = pow(sum,2);
+
+    }
+    else
+    {
+        result = pow(number1,2)*pow(number2,2);
+    }
+
+    cout<<"The result is: "<<result<<endl;
+    
+    return 0;
+}
